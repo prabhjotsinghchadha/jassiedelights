b61bba1721a454845ddefc059e94270e4a7f658e
prabhjotsinghchadha <prabhjottechs@gmail.com>
2020-09-13 19:46:26 +0530

    New Changes

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
 		//Open street  Map
-		var coord = [40.738270, -74.008911]; // <--- coordinates here
+		var coord = [11.009283, 76.957010]; // <--- coordinates here
 
 		var map = L.map('map-canvas', { scrollWheelZoom:false}).setView(coord, 19);
 
--- php/sendmail.php
+++ php/sendmail.php
@@ -1,7 +1,7 @@
 <?php
 if($_POST)
 {
-	$to_email   	= "youremail@email.com"; //Recipient email, Replace with own email here
+	$to_email   	= "jaskawal84@gmail.com"; //Recipient email, Replace with own email here
 	
 	//check if its an ajax request, exit if not
     if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
